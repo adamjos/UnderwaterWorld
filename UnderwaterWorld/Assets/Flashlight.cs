807ae1297161ad7963083a307f909c5d1bac873b
adamjos <adam_josefsson@live.com>
2020-08-16 03:40:27 +0200

    Started modify underwater lighting

---
+++ UnderwaterWorld/Assets/Flashlight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlight;
+    
+    private bool isTurnedOn = false;
+
+    private void Start()
+    {
+        flashlight = GetComponent<Light>();        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && !isTurnedOn)
+        {
+            flashlight.enabled = true;
+            isTurnedOn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && isTurnedOn)
+        {
+            flashlight.enabled = false;
+            isTurnedOn = false;
+        }
+
+    }
+
+}
--- UnderwaterWorld/Assets/Scripts/Underwater.cs
+++ UnderwaterWorld/Assets/Scripts/Underwater.cs
@@ -14,19 +14,28 @@ public class Underwater : MonoBehaviour
     private bool isCameraUnderwater;
     private Color normalColor;
     private Color underwaterColor;
+    private Color normalAmbientLightColor;
 
     public Camera fpsCamera;
+    public GameObject underwaterPostProcessGO;
+    public GameObject normalPostProcessGO;
+    public GameObject normalDirectionalLight;
+
+    private LightmapData[] lightmap_data;
 
     private Material skyboxMat;
     
     // Start is called before the first frame update
     void Start()
     {
+
+        // Save reference to existing scene lightmap data.
+        lightmap_data = LightmapSettings.lightmaps;
+
         normalColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
-
         underwaterColor = new Color(0.1177f, 0.1529f, 0.1843f, 1); // Darker
-
         //underwaterColor = new Color(0.3804f, 0.6285f, 0.8490f, 1); // Bright blue
+        normalAmbientLightColor = new Color(0.212f, 0.227f, 0.259f);
 
         skyboxMat = RenderSettings.skybox;
 
@@ -59,22 +68,35 @@ public class Underwater : MonoBehaviour
 
     void SetUnderwater ()
     {
-        RenderSettings.fogColor = underwaterColor;
+        // Disable lightmaps in scene by removing the lightmap data references
+        LightmapSettings.lightmaps = new LightmapData[] { };
+
+        normalDirectionalLight.SetActive(false);
+
+        RenderSettings.fogColor = Color.black; //underwaterColor;
         RenderSettings.fogDensity = underwaterFogDensity;
-
         RenderSettings.skybox = null;
+        RenderSettings.ambientLight = Color.black;
 
-        fpsCamera.backgroundColor = underwaterColor;
-        
-
+        fpsCamera.backgroundColor = Color.black; //underwaterColor;
+        underwaterPostProcessGO.SetActive(true);
+        normalPostProcessGO.SetActive(false);
     }
 
     void SetNormal ()
     {
+        // Reenable lightmap data in scene.
+        LightmapSettings.lightmaps = lightmap_data;
+
+        normalDirectionalLight.SetActive(true);
+
         RenderSettings.fogColor = normalColor;
         RenderSettings.fogDensity = normalFogDensity;
-
+        RenderSettings.ambientLight = normalAmbientLightColor;
         RenderSettings.skybox = skyboxMat;
+
+        underwaterPostProcessGO.SetActive(false);
+        normalPostProcessGO.SetActive(true);
     }
 
     public bool IsUnderwater ()
