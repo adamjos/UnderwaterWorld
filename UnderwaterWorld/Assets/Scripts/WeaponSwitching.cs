10e640c68a99a0c37d92a69ee76ae8c4bb962349
adamjos <adam_josefsson@live.com>
2020-09-08 18:45:34 +0200

    Fixed bug in WeaponSwitching

--- UnderwaterWorld/Assets/Scripts/WeaponSwitching.cs
+++ UnderwaterWorld/Assets/Scripts/WeaponSwitching.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class WeaponSwitching : MonoBehaviour
 {
 
     public int selectedWeapon = 0;
+    private List<int> cachedWeaponInventory = new List<int>();
+    private bool[] cachedPickedUpItems;
+    private int scrollIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         SelectWeapon();
+        UpdateWeaponInventory();
+        ItemManager.instance.OnPickedUpItem += UpdateWeaponInventory;
     }
 
     // Update is called once per frame
@@ -21,44 +27,50 @@ public class WeaponSwitching : MonoBehaviour
 
         int previousSelectedWeapon = selectedWeapon;
 
+        scrollIndex = cachedWeaponInventory.IndexOf(previousSelectedWeapon);
+
         if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            if (selectedWeapon >= ItemManager.instance.WeaponInventory.Count - 1)
+            if (scrollIndex >= cachedWeaponInventory.Count - 1)
             {
-                selectedWeapon = 0;
+                scrollIndex = 0;
             } else
             {
-                selectedWeapon++;
+                scrollIndex++;
             }
+            selectedWeapon = cachedWeaponInventory[scrollIndex];
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            if (selectedWeapon <= 0)
+            if (scrollIndex <= 0)
             {
-                selectedWeapon = ItemManager.instance.WeaponInventory.Count - 1;
+                scrollIndex = cachedWeaponInventory.Count - 1;
             } else
             {
-                selectedWeapon--;
+                scrollIndex--;
             }
+
+            selectedWeapon = cachedWeaponInventory[scrollIndex];
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1) && ItemManager.instance.pickedUpItems[1])
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && cachedPickedUpItems[1])
         {
             selectedWeapon = 1; // Grappling gun
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 3 && ItemManager.instance.pickedUpItems[2])
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 3 && cachedPickedUpItems[2])
         {
             selectedWeapon = 2; // Pistol
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 4 && ItemManager.instance.pickedUpItems[3])
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 4 && cachedPickedUpItems[3])
         {
             selectedWeapon = 3; // Rifle
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 5 && ItemManager.instance.pickedUpItems[4])
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 5 && cachedPickedUpItems[4])
         {
             selectedWeapon = 4; // Sniper rifle
         }
@@ -86,6 +98,12 @@ public class WeaponSwitching : MonoBehaviour
             i++;
         }
 
+    }
+
+    void UpdateWeaponInventory ()
+    {
+        cachedWeaponInventory = ItemManager.instance.WeaponInventory;
+        cachedPickedUpItems = ItemManager.instance.pickedUpItems;
     }
 
 }
